d6b964d417e5ece83c01cd371ff8c262640d26d4
liumingyang <852194990@qq.com>
2026-06-16 18:06:47 +0800

    保存本地修改

---
+++ Testcase/test-member-level-record/test_MemberLevelRecordPage.py
@@ -0,0 +1,36 @@
+import pytest
+from config import ADMIN_URL
+
+
+class TestMemberLevelRecordPage:
+    """获得会员等级记录分页"""
+
+    @pytest.mark.smoke
+    def test_MemberLevelRecordPage(self, api_session,auth_headers):
+        """
+        运行: TEST_ENV=dev USE_MOCK=false pytest ... -v -s
+        """
+        url = f"{ADMIN_URL}/admin-api/member/level-record/page"
+        params = {
+            "PageNo": "1",
+            "PageSize": "200",
+            "userId": "",
+            "levelId": "",
+            "creatTime": ""
+            }
+
+        resp = api_session.get(url, headers=auth_headers,params=params)
+        assert resp.status_code == 200
+        r = resp.json()
+        assert r["code"] == 0
+        #assert r["data"] == {}
+        print(r)
+
+
+
+
+
+
+
+
+#test_AppApiCooperationGetByPlatform
