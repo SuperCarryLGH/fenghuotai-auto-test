3c82a22b10faf70358d78049b55b0a0a545a2747
zhenghao <2466633777@qq.com>
2026-06-16 18:14:39 +0800

    help record + delete driver accept refactor

---
+++ Testcase/test-RecycleService-activity-help-record/test_AdminApiRecycleActivityHelpRecordDeleteList.py
@@ -0,0 +1,19 @@
+import pytest
+from config import ADMIN_URL
+from Common.loader import load_yaml
+
+DATA = load_yaml("help_record.yaml")
+
+
+class Test_AdminApiRecycleActivityHelpRecordDeleteList:
+    """批量删除活动助力明细"""
+
+    @pytest.mark.smoke
+    def test_AdminApiRecycleActivityHelpRecordDeleteList(self, api_session, auth_headers):
+        url = f"{ADMIN_URL}/admin-api/recycle/activity-help-record/delete-list"
+        body = {"ids": [DATA["id2"],DATA["id3"]]}
+        resp = api_session.delete(url, params=body, headers=auth_headers)
+        assert resp.status_code == 200
+        r = resp.json()
+        assert r["code"] == 0
+        print(r)
